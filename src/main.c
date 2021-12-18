1833d7865a13aa0dfeabd53d6b89e19e15b34ca2
tony-go <gorez.tony@gmail.com>
2021-12-18 15:48:23 +0100

    chore: add test assert

--- src/main.c
+++ src/main.c
@@ -3,12 +3,28 @@
 
 #include "string.h"
 
+char* assert_int(int a, int b);
+
 int main(void) {
   printf("string test =============\n");
 
   printf("strlen:\n");
-
-  printf("%zu\n", strlen("yo"));
+  printf("assert base length %s\n", assert_int(strlen("yo"), 2));
+  printf("assert empty string %s\n", assert_int(strlen(""), 0));
+  printf("assert null string %s\n", assert_int(strlen(NULL), 2));
 
   return EXIT_SUCCESS;
+}
+
+/**
+ * 
+ * HELPERS
+ * 
+ */
+
+char* assert_int(int a, int b) {
+  if (a == b) {
+    return "pass";
+  }
+  return "fail";
 }
