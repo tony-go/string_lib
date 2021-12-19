a1bf80a275828145610592b4695801cb29da7ade
tony-go <gorez.tony@gmail.com>
2021-12-19 21:36:26 +0100

    fix: str_comp

--- src/string_lib.c
+++ src/string_lib.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-size_t str_len(const char *s) {
-  if (NULL == s) {
+size_t str_len(const char *s)
+{
+  if (NULL == s)
+  {
     return 0;
   }
 
   size_t length = 0;
-  while (*s != 0) {
+  while (*s != 0)
+  {
     length++;
     s++;
   }
@@ -14,36 +17,32 @@ size_t str_len(const char *s) {
   return length;
 }
 
-int str_cmp(const char* s1, const char* s2) {
+int str_cmp(const char *s1, const char *s2)
+{
   int res = 0;
 
-  while (*s1 != 0 && *s2 != 0) {
-    char buffer_1 = *s1;
-    char buffer_2 = *s2;
-    // printf("%c, %c \n", buffer_1, buffer_2);
-
-    if (buffer_1 != 0 && buffer_2 == 0) {
-      printf("here a");
-      res++;
-      while (*(++s1) != 0) {
-        res++;
-      }
-      break;
-    }
-
-    if (buffer_1 == 0 && buffer_2 != 0) {
-      printf("here b");
-      res--;
-      while (*(++s2) != 0) {
-        res--;
-      }
-      break;
-    }
-
+  while (*s1 != 0 && *s2 != 0)
+  {
     s1++, s2++;
-    // printf("after %d and %d \n", *s1, *s2);
   }
 
+  if (*s1 != 0 && *s2 == 0)
+  {
+    res++;
+    while (*(++s1) != 0)
+    {
+      res++;
+    }
+  }
 
- return res;
+  if (*s1 == 0 && *s2 != 0)
+  {
+    res--;
+    while (*(++s2) != 0)
+    {
+      res--;
+    }
+  }
+
+  return res;
 }
