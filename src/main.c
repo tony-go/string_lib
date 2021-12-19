fdb6625587ac93d3bac50e9b4e76862ca9dd69c2
tony-go <gorez.tony@gmail.com>
2021-12-19 17:30:24 +0100

    first algo

--- src/main.c
+++ src/main.c
@@ -16,7 +16,12 @@ void run_tests(void) {
   printf("- assert null string %s\n", assert_int(str_len(NULL), 0));
 
   printf("-> str_cmp:\n");
-  printf("- assert equal string %s\n", assert_true(str_cmp("yo", "yo")));
+  // printf("- assert equal string %s\n", assert_int(str_cmp("yo", "yo"), 0));
+  printf("- assert equal string %s\n", assert_int(str_cmp("yoo", "yo"), 1));
+  // printf("- assert equal string %s\n", assert_int(str_cmp("yooo", "yo"), 2));
+  // printf("- assert equal string %s\n", assert_int(str_cmp("yoooo", "yo"), 3));
+  // printf("- assert equal string %s\n", assert_int(str_cmp("yo", "yoo"), -1));
+  // printf("- assert equal string %s\n", assert_int(str_cmp("yo", "yooo"), -2));
 }
 
 int main(void) {
@@ -36,6 +41,7 @@ char* assert_int(int a, int b) {
   if (a == b) {
     return "pass";
   }
+  printf("--> diff: a: %d, b: %d \n", a, b);
   return "fail";
 }
 
--- src/string_lib.c
+++ src/string_lib.c
@@ -15,11 +15,34 @@ size_t str_len(const char *s) {
 }
 
 int str_cmp(const char* s1, const char* s2) {
- printf("here \n");
+  int res = 0;
 
- if (*s1 != 0 && *s2 != 0) {
-  return 1;
- }
+  while (*s1 != 0 && *s2 != 0) {
+    char buffer_1 = *s1;
+    char buffer_2 = *s2;
+    printf("%c, %c \n", buffer_1, buffer_2);
 
- return 0;
+    if (buffer_1 != 0 && buffer_2 == 0) {
+      printf("here a");
+      res++;
+      while (*(++s1) != 0) {
+        res++;
+      }
+      break;
+    }
+
+    if (buffer_1 == 0 && buffer_2 != 0) {
+      printf("here b");
+      res--;
+      while (*(++s2) != 0) {
+        res--;
+      }
+      break;
+    }
+
+    s1++, s2++;
+  }
+
+
+ return res;
 }
