63fe7f71a31cd5a4a7644c43621d8e17d9172d74
tony-go <gorez.tony@gmail.com>
2021-12-19 17:35:56 +0100

    add logs

--- src/string_lib.c
+++ src/string_lib.c
@@ -20,7 +20,7 @@ int str_cmp(const char* s1, const char* s2) {
   while (*s1 != 0 && *s2 != 0) {
     char buffer_1 = *s1;
     char buffer_2 = *s2;
-    printf("%c, %c \n", buffer_1, buffer_2);
+    // printf("%c, %c \n", buffer_1, buffer_2);
 
     if (buffer_1 != 0 && buffer_2 == 0) {
       printf("here a");
@@ -41,6 +41,7 @@ int str_cmp(const char* s1, const char* s2) {
     }
 
     s1++, s2++;
+    // printf("after %d and %d \n", *s1, *s2);
   }
 
 
